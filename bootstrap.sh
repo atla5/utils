a05844014b5bfdcb8c870fe136a0f9b65a7c3522
atla5 <aks5238@rit.edu>
2017-10-10 00:05:10 -0400

    init: add hot corners as initial bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# initialize hot corners
+defaults write ~/Library/Preferences/com.apple.dock.plist wvous-bl-corner 4
+defaults write ~/Library/Preferences/com.apple.dock.plist wvous-bl-corner 2
